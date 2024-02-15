ad0fdb8dafd68aa04a4abaec86669a22bc52d60f
rehposolihpedoc <142445854+rehposolihpedoc@users.noreply.github.com>
2024-02-15 14:26:49 -0800

    Added Project list

---
+++ Portfolio Project/src/utils/porjectList.jsx
@@ -0,0 +1,23 @@
+
+
+const projects = [
+    {
+      title: 'Fantastic Beasts and How to Buy Them',
+      image: FantasticBeasts,
+      gitHub: '',
+      diployedApp: '',
+      description:
+        'React, MongoDB, Mongoose, Node.js, Express.js, GraphQL, MongoDB Atlas, IndexedDB, PWA, JWT, Material UI, EmailJS, Stripe, Heroku',
+    },
+    {
+      title: '',
+      image: img1,
+      gitHub: 'https://github.com/',
+      diployedApp: '',
+      description: 'API, ',
+    },
+
+  ];
+  
+  export default projects;
+  
